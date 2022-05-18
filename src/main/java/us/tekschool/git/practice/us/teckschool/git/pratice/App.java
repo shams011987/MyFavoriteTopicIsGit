914ab5344b26ddb274553d721e17248ed7953040
[shams011987] <[shamsuddin.malikzada@gmail.com]>
2022-05-18 14:33:01 -0700

    this the maven massage

---
+++ src/main/java/us/tekschool/git/practice/us/teckschool/git/pratice/App.java
@@ -0,0 +1,13 @@
+package us.tekschool.git.practice.us.teckschool.git.pratice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/us/tekschool/git/practice/us/teckschool/git/pratice/GitStepDef.java
@@ -0,0 +1,5 @@
+package us.tekschool.git.practice.us.teckschool.git.pratice;
+
+public class GitStepDef {
+
+}
